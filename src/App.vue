f9117b4e6b15af9b22528deae14638459f1b851d
TimurYusupov <timurzek95@gmail.com>
2023-01-26 11:34:08 +0100

    Die TheTwoColumsLayout-Komponente wurde erstellt

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-   <div></div>
+   <TheTwoColumnsLayout
+      :leftColumnClass="'col-md-8 bg-vue'"
+      :rightColumnClass="'col-md-4'"
+      :fullsize="true"
+   />
 </template>
 
 
 <script>
+import TheTwoColumnsLayout from "@/layouts/TheTwoColumnsLayout.vue";
+
 export default {
    name: "App",
 
    components: {
-
+      TheTwoColumnsLayout,
    },
 };
 </script>
@@ -18,4 +24,22 @@ export default {
 @import "~bootstrap/dist/css/bootstrap.min.css";
 @import "~@fortawesome/fontawesome-free/css/all.min.css";
 @import "~animate.css/animate.min.css";
+
+.bg-vue {
+   background-color: rgb(52, 73, 94);
+   color: white;
+}
+
+.bg-vue2 {
+   background-color: rgb(65, 184, 131);
+   color: white;
+}
+
+.text-vue {
+   color: rgb(52, 73, 94);
+}
+
+.text-vue2 {
+   color: rgb(65, 184, 131);
+}
 </style>
---
+++ src/layouts/TheTwoColumnsLayout.vue
@@ -0,0 +1,58 @@
+<template>
+   <div class="container-fluid" :class="containerClasses">
+      <div class="row" :class="rowClasses">
+         <div :class="leftColumnClasses">
+            <slot name="leftCol">
+               <h1>Linke Spalte</h1>
+            </slot>
+         </div>
+
+         <div :class="rightColumnClasses">
+            <slot name="rightCol">
+               <h1>Rechte Spalte</h1>
+            </slot>
+         </div>
+      </div>
+   </div>
+</template>
+
+
+<script>
+export default {
+   name: "TheTwoColumnsLayout",
+
+   props: {
+      leftColumnClass: {
+         type: String,
+         default: "col-md-8",
+      },
+      rightColumnClass: {
+         type: String,
+         default: "col-md-4"
+      },
+      fullsize: {
+         type: Boolean,
+         default: false
+      }
+   },
+
+   computed: {
+      containerClasses() {
+         return [this.fullsize ? "vh-100" : ""]; // vh-100 : 100% von dem sichtbaren Bereich des Browsers
+      },
+      rowClasses() {
+         return [this.fullsize ? "h-100" : ""]; // height 100%
+      },
+      leftColumnClasses() {
+         return [this.leftColumnClass, this.fullsize ? "h-100" : ""];
+      },
+      rightColumnClasses() {
+         return [this.rightColumnClass, this.fullsize ? "h-100" : ""];
+      }
+   }
+};
+</script>
+
+
+<style scoped>
+</style>
